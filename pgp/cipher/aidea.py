c34ac1b93a96dac461cd8ec0cbffa9ab2a52a456
Richard Mitchell <richard.j.mitchell@gmail.com>
2014-05-10 15:14:36 +0100

    Add Alleged IDEA implementation. PyCrypto support doesn't look like it'll come any time soon.

---
+++ pgp/cipher/aidea.py
@@ -0,0 +1,232 @@
+# python-pgp A Python OpenPGP implementation
+# Copyright (C) 2014 Richard Mitchell
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from Crypto.Cipher import blockalgo
+
+from pgp.cipher.base import _InternalObj
+
+
+__all__ = [
+    'AIDEACipher', 'new', 'MODE_CBC', 'MODE_CFB', 'MODE_CTR', 'MODE_EAX',
+    'MODE_ECB', 'MODE_OFB', 'MODE_OPENPGP', 'MODE_PGP',
+    ]
+
+
+def mul(x, y):
+    if x == 0:
+        return (1 - y) & 0xffff
+    elif y == 0:
+        return (1 - x) & 0xffff
+    return (x * y) % 0x10001
+
+
+def mul_inv(x):
+    x = x & 0xffff
+    if x <= 1:
+        return x
+    t1 = 0x10000 / x
+    y = 0x10001 % x
+    if y == 1:
+        return (1 - t1) & 0xffff
+    t0 = 1
+    while y != 1:
+        q = x / y
+        x = x % y
+        t0 = t0 + (q * t1)
+        if x == 1:
+            return t0
+        q = y / x
+        y = y % x
+        t1 = t1 + (q * t0)
+
+    return (1 - t1) & 0xffff
+
+
+class AIDEA(object):
+
+    def _generate_enc_keys(self, key):
+        result = list(bytearray(key)) + [0] * (52 - len(key))
+        for i in range(8, 52):
+            x = (i - 7) if ((i + 1) % 8 > 0) else (i - 15)
+            y = (i - 14) if ((i + 2) % 8 < 2) else (i - 6)
+            result[i] = ((result[x] << 9) | (result[y] >> 7)) & 0xffff
+
+        return result
+
+    def _generate_dec_keys(self, key, encryption_key_table):
+        result = [0] * 52
+        result[48] = mul_inv(encryption_key_table[0])
+        result[49] = (-encryption_key_table[1]) & 0xffff
+        result[50] = (-encryption_key_table[2]) & 0xffff
+        result[51] = mul_inv(encryption_key_table[3])
+
+        offset = 4
+        for i in range(42, -1, -6):
+            result[i + 4] = encryption_key_table[offset] & 0xffff
+            result[i + 5] = encryption_key_table[offset + 1] & 0xffff
+            result[i] = mul_inv(encryption_key_table[offset + 2])
+            result[i + 3] = mul_inv(encryption_key_table[offset + 5])
+            if i == 0:
+                result[1] = (-encryption_key_table[offset + 3]) & 0xffff
+                result[2] = (-encryption_key_table[offset + 4]) & 0xffff
+            else:
+                result[i + 2] = (-encryption_key_table[offset + 3]) & 0xffff
+                result[i + 1] = (-encryption_key_table[offset + 4]) & 0xffff
+            offset += 6
+        return result
+
+    def __init__(self, key, dummy=None):
+        self.encryption_key_table = ekt = self._generate_enc_keys(key)
+        self.decryption_key_table = self._generate_dec_keys(key, ekt)
+
+    def _bytes_to_shorts(self, block):
+        block = bytearray(block)
+        result = []
+        i = 0
+        while i < len(block):
+            result.append((block[i] << 8) + block[i + 1])
+            i += 2
+        return result
+
+    def _shorts_to_bytes(self, shorts):
+        result = bytearray()
+        for s in shorts:
+            result.append(s >> 8)
+            result.append(s & 0xff)
+        return result
+
+    def _cipher(self, block_in, key_table):
+        block = self._bytes_to_shorts(block_in)
+        offset = 0
+        for _ in range(8, 0, -1):
+            block[0] = mul(block[0], key_table[offset])
+            block[1] = (block[1] + key_table[offset + 1]) & 0xffff
+            block[2] = (block[2] + key_table[offset + 2]) & 0xffff
+            block[3] = mul(block[3], key_table[offset + 3])
+
+            t2 = block[0] ^ block[2]
+            t2 = mul(t2, key_table[offset + 4])
+            t1 = (t2 + (block[1] ^ block[3])) & 0xffff
+            t1 = mul(t1, key_table[offset + 5])
+            t2 = (t1 + t2) & 0xffff
+
+            block[0] ^= t1
+            block[3] ^= t2
+            t2 ^= block[1]
+            block[1] = block[2] ^ t1
+            block[2] = t2
+            offset += 6
+
+        x1 = mul(block[0], key_table[offset])
+        x2 = (block[2] + key_table[offset + 1]) & 0xffff
+        x3 = (block[1] + key_table[offset + 2]) & 0xffff
+        x4 = mul(block[3], key_table[offset + 3])
+
+        block_out = self._shorts_to_bytes([x1, x2, x3, x4])
+        return bytes(block_out)
+
+    def encrypt(self, block_in):
+        return self._cipher(block_in, self.encryption_key_table)
+
+    def decrypt(self, block_in):
+        return self._cipher(block_in, self.decryption_key_table)
+
+
+class _AIDEAObj(_InternalObj):
+
+    @classmethod
+    def _create_impl(cls, key):
+        impl = AIDEA(key)
+        return impl
+
+    block_size = 8
+    key_size = (16,)
+
+
+# Mock out a PyCrypt style cipher using camcrypt underneath
+class AIDEACipher(blockalgo.BlockAlgo):
+
+    def __init__(self, key, *args, **kwargs):
+        blockalgo.BlockAlgo.__init__(self, _AIDEAObj, key, *args, **kwargs)
+
+
+def new(key, *args, **kwargs):
+    """Create a new Alleged IDEA cipher
+
+    :Parameters:
+      key : byte string
+        The secret key to use in the symmetric cipher.
+        Its length may vary from 5 to 16 bytes.
+    :Keywords:
+      mode : a *MODE_** constant
+        The chaining mode to use for encryption or decryption.
+        Default is `MODE_ECB`.
+      IV : byte string
+        (*Only* `MODE_CBC`, `MODE_CFB`, `MODE_OFB`, `MODE_OPENPGP`).
+
+        The initialization vector to use for encryption or decryption.
+
+        It is ignored for `MODE_ECB` and `MODE_CTR`.
+
+        For `MODE_OPENPGP`, IV must be `block_size` bytes long for
+        encryption and `block_size` +2 bytes for decryption (in the
+        latter case, it is actually the *encrypted* IV which was
+        prefixed to the ciphertext). It is mandatory.
+
+        For all other modes, it must be 8 bytes long.
+      nonce : byte string
+        (*Only* `MODE_EAX`).
+        A mandatory value that must never be reused for any other
+        encryption. There are no restrictions on its length, but it is
+        recommended to use at least 16 bytes.
+      counter : callable
+        (*Only* `MODE_CTR`). A stateful function that returns the next
+        *counter block*, which is a byte string of `block_size` bytes.
+        For better performance, use `Crypto.Util.Counter`.
+      mac_len : integer
+        (*Only* `MODE_EAX`). Length of the MAC, in bytes.
+        It must be no larger than 8 (which is the default).
+      segment_size : integer
+        (*Only* `MODE_CFB`).The number of bits the plaintext and
+        ciphertext are segmented in.
+        It must be a multiple of 8. If 0 or not specified, it will be
+        assumed to be 8.
+
+    :Return: an `CamelliaCipher` object
+    """
+    return AIDEACipher(key, *args, **kwargs)
+
+
+#: Electronic Code Book (ECB). See `blockalgo.MODE_ECB`.
+MODE_ECB = 1
+#: Cipher-Block Chaining (CBC). See `blockalgo.MODE_CBC`.
+MODE_CBC = 2
+#: Cipher FeedBack (CFB). See `blockalgo.MODE_CFB`.
+MODE_CFB = 3
+#: This mode should not be used.
+MODE_PGP = 4
+#: Output FeedBack (OFB). See `blockalgo.MODE_OFB`.
+MODE_OFB = 5
+#: CounTer Mode (CTR). See `blockalgo.MODE_CTR`.
+MODE_CTR = 6
+#: OpenPGP Mode. See `blockalgo.MODE_OPENPGP`.
+MODE_OPENPGP = 7
+#: EAX Mode. See `blockalgo.MODE_EAX`.
+MODE_EAX = 9
+#: Size of a data block (in bytes)
+block_size = _AIDEAObj.block_size
+#: Size of a key (in bytes)
+key_size = _AIDEAObj.key_size
---
+++ pgp/cipher/tests/test_aidea.py
@@ -0,0 +1,121 @@
+# python-pgp A Python OpenPGP implementation
+# Copyright (C) 2014 Richard Mitchell
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from Crypto.SelfTest.Cipher.common import make_block_tests
+
+from pgp.cipher import aidea
+
+
+# This is a list of (plaintext, ciphertext, key) tuples.
+test_data = [
+
+        # Project NESSIE test vectors
+        # http://bit.ly/1kMAzsR
+        ('0000000000000000', 'b1f5f7f87901370f',
+         '80000000000000000000000000000000',
+         'IDEA NESSIE, Set 1, vector 0'),
+
+        ('0000000000000000', 'b3927dffb6358626',
+         '40000000000000000000000000000000',
+         'IDEA NESSIE, Set 1, vector 1'),
+
+        ('8000000000000000', '8001000180008000',
+         '00000000000000000000000000000000',
+         'IDEA NESSIE, Set 2, vector 0'),
+
+        ('4000000000000000', 'c00180014000c000',
+         '00000000000000000000000000000000',
+         'IDEA NESSIE, Set 2, vector 1'),
+
+        ('0000000000000000', '0001000100000000',
+         '00000000000000000000000000000000',
+         'IDEA NESSIE, Set 3, vector 0'),
+
+        ('0101010101010101', 'e3f8aff7a3795615',
+         '01010101010101010101010101010101',
+         'IDEA NESSIE, Set 3, vector 1'),
+
+        ('0011223344556677', 'f526ab9a62c0d258',
+         '000102030405060708090a0b0c0d0e0f',
+         'IDEA NESSIE, Set 4, vector 0'),
+
+        ('ea024714ad5c4d84', 'c8fb51d3516627a8',
+         '2bd6459f82c5b300952c49104881ff48',
+         'IDEA NESSIE, Set 4, vector 1'),
+
+        ('78071ee87f0130e8', '0000000000000000',
+         '80000000000000000000000000000000',
+         'IDEA NESSIE, Set 5, vector 0'),
+
+        ('98aa167965b52792', '0000000000000000',
+         '40000000000000000000000000000000',
+         'IDEA NESSIE, Set 5, vector 1'),
+
+        ('8001000180008000', '8000000000000000',
+         '00000000000000000000000000000000',
+         'IDEA NESSIE, Set 6, vector 0'),
+
+        ('c00180014000c000', '4000000000000000',
+         '00000000000000000000000000000000',
+         'IDEA NESSIE, Set 6, vector 1'),
+
+        ('0001000100000000', '0000000000000000',
+         '00000000000000000000000000000000',
+         'IDEA NESSIE, Set 7, vector 0'),
+
+        ('6d33179ce8b3c1fa', '0101010101010101',
+         '01010101010101010101010101010101',
+         'IDEA NESSIE, Set 7, vector 1'),
+
+        ('db2d4a92aa68273f', '0011223344556677',
+         '000102030405060708090a0b0c0d0e0f',
+         'IDEA NESSIE, Set 8, vector 0'),
+
+        ('f129a6601ef62a47', 'ea024714ad5c4d84',
+         '2bd6459f82c5b300952c49104881ff48',
+         'IDEA NESSIE, Set 8, vector 1'),
+
+        # from PyCA's cryptography package:
+        # http://bit.ly/1j3pnGT
+        ('45cf12964fc824ab76616ae2f4bf0822',
+         '2cb10d22ac22a375c0021ab6732936c1',
+         '1f8e4973953f3fb0bd6b16662e9a3c17',
+         'PyCA, CBC',
+         dict(mode='CBC', iv='2fe2b333ceda8f98')),
+
+        ('4b5a872260293312eea1a570fd39c788',
+         '5d9c48bf7dc115f28e153dc93dfcff96',
+         '085b8af6788fa6bc1a0b47dcf50fbd35',
+         'PyCA, CFB',
+         dict(mode='CFB', iv='58cb2b12bb52c6f1')),
+
+        ('81883f22165282ba6a442a8dd2a768d4',
+         '770e7b0eacc089b7eef410d98d886e9e',
+         'd7d57bd847154af9722a8df096e61a42',
+         'PyCA, CFB',
+         dict(mode='OFB', iv='fdde201c91e401d9')),
+    ]
+
+
+def get_testcases(config={}):
+    testcases = make_block_tests(aidea, "AIDEA", test_data)
+    return testcases
+
+
+def test_aidea():
+    for testcase in get_testcases():
+        # Hack for Nose
+        yield getattr(testcase, testcase._testMethodName)
