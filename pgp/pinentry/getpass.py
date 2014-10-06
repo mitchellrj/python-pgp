dfaab5eb466d96eb4113f1779fe1774b15ea9e32
Richard Mitchell <richard.j.mitchell@gmail.com>
2014-10-06 17:00:18 +0100

    Beginnings of pin-entry implementations.

---
+++ pgp/pinentry/getpass.py
@@ -0,0 +1,67 @@
+# python-pgp A Python OpenPGP implementation
+# Copyright (C) 2014 Richard Mitchell
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from getpass import getpass
+import sys
+
+
+class GetPassPinentry(object):
+
+    def __init__(self):
+        self._cache = {}
+
+    def get_passphrase(self, prompt=None, error_message=None,
+                       description=None, cache_id=None, use_cache=True, no_ask=False,
+                       qualitybar=False, repeat=0, stream=sys.stdout):
+        if prompt in (None, 'X'):
+            # Default prompt
+            prompt = 'PIN?'
+        if error_message in (None, 'X'):
+            error_message = 'Error.'
+        if description in (None, 'X'):
+            description = ''
+        if cache_id == 'X':
+            cache_id = None
+
+        if use_cache and cache_id and cache_id in self._cache:
+            return self._cache[cache_id]
+
+        if no_ask:
+            raise KeyError(cache_id)
+
+        if prompt:
+            print(prompt, file=stream)
+        if description:
+            print(description, file=stream)
+
+        match = False
+        while not match:
+            match = True
+            if error_message:
+                print(error_message, file=stream)
+            passphrase = getpass('Password: ', stream=stream)
+            for _i in range(repeat):
+                if getpass('Confirm: ', stream=stream) != passphrase:
+                    match = False
+                    error_message = 'Passwords do not match.'
+
+        if use_cache and cache_id:
+            self._cache[cache_id] = passphrase
+        return passphrase
+
+    def clear_passphrase(self, cache_id):
+        while cache_id in self._cache:
+            del self._cache[cache_id]
---
+++ pgp/pinentry/gpg_agent.py
@@ -0,0 +1,100 @@
+# python-pgp A Python OpenPGP implementation
+# Copyright (C) 2014 Richard Mitchell
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import socket
+from urllib.parse import quote
+from urllib.parse import unquote
+
+
+class GPGAgentPinentry(object):
+
+    def __init__(self, name=None, socket_path=None):
+        if name is None:
+            name = __name__
+        if socket_path is None:
+            socket_path = os.getenv('GPG_AGENT_INFO', ':::').split(':', 1)[0]
+        self.socket_path = socket_path
+        self._sock = None
+
+    def connect(self):
+        self._sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._sock.connect(self.socket_path)
+        # Get welcome line
+        self.get_response()
+
+    def disconnect(self):
+        self._sock.close()
+
+    def send(self, data):
+        if isinstance(data, str):
+            data = data.encode('latin-1')
+        self._sock.send(data + b'\n')
+
+    def get_response(self):
+        buf = b''
+        lines = []
+        while 1:
+            byte = self._sock.recv(1)
+            if byte == b'\n':
+                lines.append(buf)
+                if buf[:2] == b'OK':
+                    break
+                elif buf[:3] == b'ERR':
+                    raise ValueError(buf[4:].decode('latin-1'))
+                buf = b''
+            else:
+                buf += byte
+        return lines
+
+    def get_passphrase(self, prompt=None, error_message=None,
+                       description=None, cache_id=None, use_cache=True, no_ask=False,
+                       qualitybar=False, repeat=0):
+        self.connect()
+        params = '--data'
+        if no_ask:
+            params += ' --no-ask'
+        if qualitybar:
+            params += ' --qualitybar'
+        if repeat == 1:
+            params += ' --repeat'
+        elif repeat > 1:
+            params += ' --repeat={0}'.format(repeat)
+        if cache_id:
+            params += ' {0}'.format(quote(cache_id))
+        else:
+            params += ' X'
+        if error_message:
+            params += ' {0}'.format(quote(error_message))
+        else:
+            params += ' X'
+        if prompt:
+            params += ' {0}'.format(quote(prompt))
+        else:
+            params += ' X'
+        if description:
+            params += ' {0}'.format(quote(description))
+        else:
+            params += ' X'
+        self.send('GET_PASSPHRASE {0}'.format(params))
+        response = self.get_response()
+        self.disconnect()
+        return unquote(response[0][2:].decode('latin-1'))
+
+    def clear_passphrase(self, cache_id):
+        self.connect()
+        self.send('CLEAR_PASSPHRASE {0}'.format(quote(cache_id)))
+        self.disconnect()
